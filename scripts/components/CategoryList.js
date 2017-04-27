ca7f5800d978a83ce0bcc35eba144aa126a00e1e
traustid <traustid@gmail.com>
2017-04-27 11:08:59 +0200

    Flyttade utils/sagenKartaCategories.js till ISOF-React-modules

--- scripts/components/CategoryList.js
+++ scripts/components/CategoryList.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import _ from 'underscore';
 
-import categories from './../utils/sagenkartaCategories.js';
+import categories from './../../ISOF-React-modules/utils/sagenkartaCategories.js';
 
 export default class CategoryMenu extends React.Component {
 	constructor(props) {
