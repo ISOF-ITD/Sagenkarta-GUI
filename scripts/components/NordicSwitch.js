d66cbae5045061dd2061424cbb15f4e183eb36ea
traustid <traustid@gmail.com>
2017-08-10 14:20:38 +0200

    NordicSwitch interaktion.

--- scripts/components/NordicSwitch.js
+++ scripts/components/NordicSwitch.js
@@ -5,11 +5,24 @@ import { hashHistory } from 'react-router';
 export default class NordicSwitch extends React.Component {
 	constructor(props) {
 		super(props);
+
+		this.menuButtonClick = this.menuButtonClick.bind(this);
+
+		this.state = {
+			selected: 'swedish'
+		};
+	}
+
+	menuButtonClick(event) {
+		this.setState({
+			selected: event.currentTarget.dataset.value
+		});
 	}
 
 	render() {
 		return <div className="nordic-switch-wrapper map-floating-control">
-			NordicSwitch
+			<a onClick={this.menuButtonClick} data-value="swedish" className={this.state.selected == 'swedish' ? 'selected' : ''}>Svenska sägner</a>
+			<a onClick={this.menuButtonClick} data-value="nordic" className={this.state.selected == 'nordic' ? 'selected' : ''}>Nordiska sägner</a>
 		</div>;
 	}
 
