9367c524819a7270ad3ad679b782cca82b177342
Unknown <traustid@gmail.com>
2017-12-18 13:43:45 +0100

    langData flyttat till projekt nivå

--- scripts/config.js
+++ scripts/config.js
@@ -20,8 +20,8 @@ export default {
 	appUrl: 'http://www4.sprakochfolkminnen.se/sagner/',
 	siteUrl: 'http://www.sprakochfolkminnen.se/om-oss/kartor/sagenkartan.html',
 
-	apiUrl: 'http://frigg-test.sprakochfolkminnen.se/sagendatabas/api/es/',
-	djangoApiUrl: 'http://frigg-test.sprakochfolkminnen.se/sagendatabas/api/'
+	apiUrl: 'http://frigg.sprakochfolkminnen.se/sagendatabas/api/es/',
+	restApiUrl: 'http://frigg.sprakochfolkminnen.se/sagendatabas/api/'
 //	apiUrl: 'http://uuc-isof004-t.its.uu.se/sagendatabas/api/es/'
 //	apiUrl: 'http://uuc-isof004-t.its.uu.se/sagendatabas/api/'
 //	apiUrl: 'http://localhost:8000/sagenkarta/'
